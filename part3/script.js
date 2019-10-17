894492601a1bd0eff5a6ec209f5c7a7c12f3d5c8
Charles de Dives <noel@lacapsule.academy>
2019-10-17 16:32:38 +0200

    Mymails

---
+++ part3/script.js
@@ -0,0 +1,45 @@
+/* -------- CODING PART WITH DOM INSTRUCTIONS --------------------------------------------------------------------*/
+
+// Calculating the number of messages 
+var messagesCount = document.getElementsByTagName('p').length
+
+// updating the counter (with id='count')
+document.getElementById('count').textContent = messagesCount
+
+
+// For each paragraph 'p'
+for(var i=0; i<document.getElementsByTagName('p').length; i++) {
+
+      // 1) Listening for the click on a trash
+      document.getElementsByClassName('trash')[i].addEventListener("click", 
+       function(){ 
+
+          // 2) deleting the parent of the trash --> the entire Row 
+          this.parentNode.remove();
+
+          // 3)- Updating the count again
+
+            // We need the acquisition of the messages numbers again
+            messagesCount = document.getElementsByTagName('p').length
+
+            // updating counter once more
+            document.getElementById('count').textContent = messagesCount
+       }
+      );
+};   
+
+
+  document.getElementById('btn-blue').addEventListener("click", 
+  function() {
+    console.log(this.previousElementSibling.value);
+    var newText = this.previousElementSibling.value;
+    console.log(newText);
+    var newRow = document.createElement("div");
+    document.body.appendChild(newRow);
+
+  }
+);
+/* -------------------------------------------------------------------------------------------------------*/
+
+
+
